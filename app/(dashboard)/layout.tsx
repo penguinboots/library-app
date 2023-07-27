f8b428b36be628e786e14420dc5bb6ec7306b379
penguinboots <sabrina.ynw@gmail.com>
2023-07-27 13:50:52 -0700

    move navbar to layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,11 @@
+import { Navbar } from "@/components/Navbar";
 import React from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="h-full overflow-auto">
       <div className="mx-auto max-w-screen-xl h-full w-full">
+        <Navbar />
         {children}
       </div>
     </main>
