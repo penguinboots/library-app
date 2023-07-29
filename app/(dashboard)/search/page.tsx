f6b10b03bce9406b010a89793860d67dc7ee5bd5
penguinboots <sabrina.ynw@gmail.com>
2023-07-28 18:00:31 -0700

    build search bar

--- app/(dashboard)/search/page.tsx
+++ app/(dashboard)/search/page.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+"use client";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  search: z.string().min(1, {
+    message: "Enter a search term!",
+  }),
+});
 
 const SearchPage = () => {
-  return (
-    <div>SearchPage</div>
-  )
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: "",
+    },
+  });
 
-export default SearchPage
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      console.log(values.search);
+      form.reset();
+    } catch (error: any) {
+      //
+    }
+  };
+
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          autoComplete="off"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="rounded-lg border p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+        >
+          <FormField
+            name="search"
+            render={({ field }) => (
+              <FormItem className="col-span-12 lg:col-span-10">
+                <FormControl>
+                  <Input
+                    className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                    placeholder="Search..."
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="col-span-12 lg:col-span-2 w-full">
+            Search
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default SearchPage;
